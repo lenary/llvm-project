b7280ff377111ead35a06e11017e9b286f3bd832
Sam Elliott <aelliott@qti.qualcomm.com>
2025-08-14 21:55:12 -0700

    code formatting

--- llvm/lib/MC/MCExpr.cpp
+++ llvm/lib/MC/MCExpr.cpp
@@ -362,9 +362,8 @@ static void attemptToFoldSymbolOffsetDifference(const MCAssembler *Asm,
           return;
       }
       const auto *RF = dyn_cast<MCRelaxableFragment>(F);
-      if (RF && RF->isLinkerRelaxable()) {
+      if (RF && RF->isLinkerRelaxable())
         return;
-      }
       if (&*F == FA) {
         // If FA and FB belong to the same subsection, the loop will find FA and
         // we can resolve the difference.
